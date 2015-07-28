25ba117654923751cd5e621c94b98091cf95ab2f
limitium <limitium@gmail.com>
2015-07-28 20:04:58 +0300

    User from db

---
+++ backend/src/AppBundle/Entity/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use IMAG\LdapBundle\User\LdapUser;
+
+/**
+ * User
+ */
+class User extends LdapUser
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $dbUsername;
+
+
+    /**
+     * Get id
+     *
+     * @return string 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dbUsername
+     *
+     * @param string $dbUsername
+     * @return User
+     */
+    public function setDbUsername($dbUsername)
+    {
+        $this->dbUsername = $dbUsername;
+
+        return $this;
+    }
+
+    /**
+     * Get dbUsername
+     *
+     * @return string 
+     */
+    public function getDbUsername()
+    {
+        return $this->dbUsername;
+    }
+}
